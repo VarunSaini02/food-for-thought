cef2f03e5c580f3fab0719263feea58d93447a65
Varun Saini <varun.saini@gmail.com>
2022-07-24 04:02:01 -0500

    Higher file limit

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = require("./router");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -24,6 +25,8 @@ mongoose
     console.log(err);
   });
 
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
